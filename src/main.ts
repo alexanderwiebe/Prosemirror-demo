b682c7b07c8f5aa462e7a1a22bf2defde7da9189
Alexander Wiebe <alexanderjwiebe@gmail.com>
2025-03-05 23:49:38 -0500

    have indexes working and a debugging tool for prosemirror

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import './style.css'
-import {EditorState, Plugin} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-import {Schema, DOMParser} from "prosemirror-model"
-import {schema} from "prosemirror-schema-basic"
-import {addListNodes} from "prosemirror-schema-list"
-import {exampleSetup} from "prosemirror-example-setup"
+import { exampleSetup } from "prosemirror-example-setup";
+import { DOMParser, Schema } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { EditorState, Plugin } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import "./style.css";
+
+function isIndexWithinRange(from: number, to: number, index: number): boolean {
+  return (index >= from && index <= to) || (from === to && index === from);
+}
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 const mySchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks
+  marks: schema.spec.marks,
 });
 
 const myPlugin = new Plugin({
@@ -22,23 +26,108 @@ const myPlugin = new Plugin({
     apply(tr, value, oldState, newState) {
       console.log("Transaction applied");
       return value;
-    }
-  }
+    },
+  },
+});
+
+const debugPosition = new Plugin({
+  state: {
+    init(config, instance) {
+      return {};
+    },
+    apply(tr, value, oldState, newState) {
+      const { from, to } = newState.selection;
+      document.querySelector(
+        "#cursorPosition"
+      )!.textContent = `Cursor position: from ${from} to ${to}`;
+
+      const posIndexRow = document.querySelector("#posIndex");
+      const posValueRow = document.querySelector("#posValue");
+      if (posIndexRow && posValueRow) {
+        posIndexRow.innerHTML = "";
+        posValueRow.innerHTML = "";
+      }
+      newState.doc.descendants((node, pos) => {
+        if (node.type.name === "text") {
+          node.textContent.split("").forEach((char, index, { length }) => {
+            const tdIndex = document.createElement("td");
+            tdIndex.textContent = (pos + index).toString();
+
+            const tdValue = document.createElement("td");
+            tdValue.textContent = char;
+
+            if (isIndexWithinRange(from, to, pos + index)) {
+              tdIndex.style.backgroundColor = "yellow";
+              tdValue.style.backgroundColor = "yellow";
+            }
+
+            posIndexRow?.appendChild(tdIndex);
+            posValueRow?.appendChild(tdValue);
+            if (index === length - 1) {
+              const tdIndex = document.createElement("td");
+              tdIndex.textContent = (pos + index + 1).toString();
+
+              const tdValue = document.createElement("td");
+              tdValue.textContent = `␣`;
+
+              if (isIndexWithinRange(from, to, pos + index + 1)) {
+                tdIndex.style.backgroundColor = "yellow";
+                tdValue.style.backgroundColor = "yellow";
+              }
+
+              posIndexRow?.appendChild(tdIndex);
+              posValueRow?.appendChild(tdValue);
+            }
+          });
+        } else {
+          const tdIndex = document.createElement("td");
+          tdIndex.textContent = pos.toString();
+
+          const tdValue = document.createElement("td");
+          tdValue.textContent = node.type.name;
+
+          if (isIndexWithinRange(from, to, pos)) {
+            tdIndex.style.backgroundColor = "yellow";
+            tdValue.style.backgroundColor = "yellow";
+          }
+
+          posIndexRow?.appendChild(tdIndex);
+          posValueRow?.appendChild(tdValue);
+        }
+
+        return true; // Continue traversing
+      });
+      document.querySelector("#prosemirrorJson")!.textContent = JSON.stringify(
+        newState.toJSON(),
+        null,
+        2
+      );
+
+      return value;
+    },
+  },
 });
 
 const view = new EditorView(document.querySelector("#editor"), {
   state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")!),
-    plugins: exampleSetup({schema: mySchema}).concat(myPlugin)
-  })
+    doc: DOMParser.fromSchema(mySchema).parse(
+      document.querySelector("#content")!
+    ),
+    plugins: exampleSetup({ schema: mySchema }).concat([
+      myPlugin,
+      debugPosition,
+    ]),
+  }),
 });
 
-const instertTransaction = view.state.tr.insertText('aaaaa', 0);
-console.log(instertTransaction);
-view.dispatch(instertTransaction);
-const instertTransaction2 = view.state.tr.insertText('bbbbb', 0);
-view.dispatch(instertTransaction2);
-const instertTransaction3 = view.state.tr.insertText('ccccc', 0);
-view.dispatch(instertTransaction3);
+const insertTransaction1 = view.state.tr.insertText("aaaaa", 0);
+view.dispatch(insertTransaction1);
 
-console.log(view);
+const insertTransaction = view.state.tr.insertText("**", 0);
+view.dispatch(insertTransaction);
+
+const insertTransaction2 = view.state.tr.insertText("bbbbb", 0);
+view.dispatch(insertTransaction2);
+
+const insertTransaction3 = view.state.tr.insertText("ccccc", 0);
+view.dispatch(insertTransaction3);
